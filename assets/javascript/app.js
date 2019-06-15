cb467b2337c67c8bcc9acc29be6e09a457fabca9
Paul Unsted <ed__84@hotmail.com>
2019-06-15 05:53:19 -0400

    Scripted an onclick event to handle the start of the game and implemented a dummy radio selection as an example to work off of.

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    // Variables to contain the number of correct or incorrect guesses.
+
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+
+    // Now an .on click event to clear away the starting button and display the questions, "the done" button and start the timer.
+    
+    $("#startButt").on("click", function () {
+
+        // Avoid using document.write or writeln, they don't only wipe out your body but wipe out everything in the head as well.
+
+        // document.writeln("<h1>Videogame Trivia</h1><div id='startButt'><h2>Start</h2></div>");
+
+        // document.write("<h1>Videogame Trivia</h1><div id='startButt'><h2>Start</h2></div>");
+
+        // --------------------------------------
+
+        // However, the line below works like a charm.
+
+        this.innerHTML = "<h2>Hello World!</h2>";
+
+        console.log("here we go");
+
+    });
+
+
+
+});
