01d0c28a4d2087f8f91c0ba1272b289e428bc0ea
Paul Unsted <ed__84@hotmail.com>
2019-06-17 05:21:14 -0400

    Did some mild css styling, played around with delivering multiple radio selections upon a button being pressed. Progress is good.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,15 +17,25 @@ $(document).ready(function () {
 
         // --------------------------------------
 
-        // However, the line below works like a charm.
+        // So, the below works in that it first fills "this" with the first .innerHTML and logs the first consolelog, but then it replaces that content with the second innerHTML and then logs the second statement to console.
 
-        this.innerHTML = "<h2>Hello World!</h2>";
+        // It looks like I'm going to have to create a series of empty divs, and then use get their individual ids on click (?) so that i can then .innerHTML various radio selections for questions.
 
+        this.innerHTML = "<h2>Hello World!</h2><form id='ques1' action=''><input type='radio' name='gender' value='guess1'> Guess 1<input type='radio' name='gender' value='guess2'> Guess 2<input type='radio' name='gender' value='guess3'> Guess 3<input type='radio' name='gender' value='guess4'> Guess 4</form>";
+       
         console.log("here we go");
 
-        for (i=0; i < 101; i++){
-            console.log(i, "har-d-har-har");
-        };
+        this.innerHTML = "<h2>Hello Again World!</h2><form id='ques2' action=''><input type='radio' name='gender' value='guess1'> Guess 1<input type='radio' name='gender' value='guess2'> Guess 2<input type='radio' name='gender' value='guess3'> Guess 3<input type='radio' name='gender' value='guess4'> Guess 4</form>";
+
+        console.log("here we go again");
+
+        // Just a fiddle. Speaking of which I should make a dedicated fiddle project to experiment with, but that's for another time.
+
+        // for (i=0; i < 101; i++){
+        //     console.log(i, "har-d-har-har");
+        // };
+
+        // Last step for the Start button is to create a Done button.
 
     });
 
