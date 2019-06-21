ba5a782907ec146b2e675f4492e2bfdd4d974ca8
Paul Unsted <ed__84@hotmail.com>
2019-06-20 23:30:10 -0400

    Game is complete, just need to iron out a few wrinkles and it should run nice, tight and clean.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
 
     // Variables to contain the number of correct or incorrect guesses.
+    
+    var n = 20;
 
-    var correctAnswers = 0;
-    var incorrectAnswers = 0;
+    var right = 0;
 
+    var wrong = 0;
+
+    var unAnswered = 0;
+    
     var counter = document.getElementById("CD");
-
-    var questing = document.getElementById("quests");
-
+    
     var question1 = document.getElementById("q1");
     var question2 = document.getElementById("q2");
     var question3 = document.getElementById("q3");
@@ -17,11 +20,17 @@ $(document).ready(function () {
     var question6 = document.getElementById("q6");
     var question7 = document.getElementById("q7");
     var question8 = document.getElementById("q8");
-
+    
     var stopButt = document.getElementById("stopButt");
-
+    
+    var correctAnswers = document.getElementById("correct");
+    var incorrectAnswers = document.getElementById("incorrect");
+    var unAnswered = document.getElementById("unSelected");
+    
     function purge () {
-        
+
+        n = 0;
+        counter.innerHTML = "";
         question1.innerHTML = "";
         question2.innerHTML = "";
         question3.innerHTML = "";
@@ -35,27 +44,30 @@ $(document).ready(function () {
 
     }
 
+    
     // Now an .on click event to clear away the starting button and display the questions, "the done" button and start the timer.
     
     $("#startButt").click(function(){
-
-        // Whatever questions are or aren't answered are sorted out if you don't click on the done button and the time runs out.
-
-        // setTimeout(purge, 1000 *10);
-        counter.innerHTML = "<h2>Time left: " + "20" + "</h2><br>";
-
-        var n = 20;
-        setTimeout(countDown,1000);
-
-        function countDown(){
-        n--;
-        if(n > 0){
-            setTimeout(countDown,1000);
-        }
-        console.log(n);
-        counter.innerHTML = "<h2>Time left: " + n + "</h2><br>";
         
+        
+        counter.innerHTML = "<h2>Time left: " + "20" + "</h2><br>";
+        
+        setTimeout(countDown,1000);
+        
+        function countDown(){
+            if(n > 0){
+                n--;
+                setTimeout(countDown,1000);
+            }
+            console.log(n);
+            counter.innerHTML = "<h2>Time left: " + n + "</h2><br>";
+            
         }
+        
+            if ( n === 0){
+            purge();
+            console.log("purged");
+            }
 
         // Avoid using document.write or writeln, they don't only wipe out your body but wipe out everything in the head as well.
 
@@ -71,25 +83,25 @@ $(document).ready(function () {
 
         this.innerHTML = "";
 
-        questing.innerHTML = 
+        // questing.innerHTML = 
 
-        question1.innerHTML = "<h2>What game almost destroyed the western videogame market in the 20th century?</h2><form id='ques1' action='' method='get'><input type='radio' name='gender' value='guess1'> Pong <input type='radio' name='gender'> E.T. <input type='radio' name='gender'> Asteroids <input type='radio' name='gender'> Joust </form><br>"
+        question1.innerHTML = "<h2>What game almost destroyed the western videogame market in the 20th century?</h2><form id='ques1' method='get'><input type='radio' name='pong'> Pong <input type='radio' name='et'> E.T. <input type='radio' name='asteroids'> Asteroids <input type='radio' name='joust'> Joust <input type='submit' value='press me bitch'><br>"
 
-        question2.innerHTML = "<h2>What is the name of the main character in the Legend of Zelda?</h2><form id='ques2' action='' method='get'><input type='radio' name='gender'> Lonk <input type='radio' name='gender'> Epona <input type='radio' name='gender'> Link <input type='radio' name='gender'> Zelda </form><br>";
+        question2.innerHTML = "<h2>What is the name of the main character in the Legend of Zelda?</h2><form id='ques2' method='get'><input type='radio' name='lonk'> Lonk <input type='radio' name='epona'> Epona <input type='radio' name='link'> Link <input type='radio' name='zelda'> Zelda </form><br>";
        
-        question3.innerHTML = "<h2>In what year was the Nintendo Entertainment System released in North America?</h2><form id='ques3' method='get' action=''><input type='radio' name='gender'> 1982 <input type='radio' name='gender'> 1986 <input type='radio' name='gender'> 2000 <input type='radio' name='gender'> 1985 </form><br>";
+        question3.innerHTML = "<h2>In what year was the Nintendo Entertainment System released in North America?</h2><form id='ques3' method='get'><input type='radio' name='1982'> 1982 <input type='radio' name='1986'> 1986 <input type='radio' name='2000'> 2000 <input type='radio' name='1985'> 1985 </form><br>";
 
-        question4.innerHTML = "<h2>What game made its debut in the 1989 film The Wizard?</h2><form id='ques4' action='' method='get'><input type='radio' name='gender'> Super Mario 3 <input type='radio' name='gender'> Kirby's Adventure <input type='radio' name='gender'> Contra <input type='radio' name='gender'> Aterburner </form><br>";
+        question4.innerHTML = "<h2>What game made its debut in the 1989 film The Wizard?</h2><form id='ques4' method='get'><input type='radio' name='super_mario_3'> Super Mario 3 <input type='radio' name='kirbys_adventure'> Kirby's Adventure <input type='radio' name='contra'> Contra <input type='radio' name='afterburner'> Aterburner </form><br>";
 
-        question5.innerHTML = "<h2>On what console was Streets of Rage released?</h2><form id='ques5' action='' method='get'><input type='radio' name='gender'> Genesis <input type='radio' name='gender'> Super Nintendo <input type='radio' name='gender'> Playstation <input type='radio' name='gender'> 3DO </form><br>";
+        question5.innerHTML = "<h2>On what console was Streets of Rage released?</h2><form id='ques5' method='get'><input type='radio' name='genesis'> Genesis <input type='radio' name='super_nintendo'> Super Nintendo <input type='radio' name='playstation'> Playstation <input type='radio' name='3do'> 3DO </form><br>";
 
-        question6.innerHTML = "<h2>What popular series of games is Hideo Kojima responsible for?</h2><form id='' action='' method='get'><input type='radio' name='gender'> Castlevania <input type='radio' name='gender'> Sonic the Hedgehog <input type='radio' name='gender'> Pokemon <input type='radio' name='gender'> Metal Gear </form><br>";
+        question6.innerHTML = "<h2>What popular series of games is Hideo Kojima responsible for?</h2><form id='ques6' method='get'><input type='radio' name='castlevania'> Castlevania <input type='radio' name='sonic'> Sonic the Hedgehog <input type='radio' name='pokemon'> Pokemon <input type='radio' name='metal_gear'> Metal Gear </form><br>";
 
-        question7.innerHTML = "<h2>What studio published Warcraft?</h2><form id='' action='' method='get'><input type='radio' name='gender'> Bioware <input type='radio' name='gender'> Blizzard <input type='radio' name='gender'> Black Isle <input type='radio' name='gender'> Electronic Arts </form><br>";
+        question7.innerHTML = "<h2>What studio published Warcraft?</h2><form id='ques7' method='get'><input type='radio' name='bioware'> Bioware <input type='radio' name='blizzard'> Blizzard <input type='radio' name='black_isle'> Black Isle <input type='radio' name='ea'> Electronic Arts </form><br>";
 
-        question8.innerHTML = "<h2>How much was the global videogame market worth in 2018?</h2><form id='' action='' method='get'><input type='radio' name='gender'> $569 million <input type='radio' name='gender'> $799 billion <input type='radio' name='gender'> $135 billion <input type='radio' name='gender'> $61 billion </form><br>";
+        question8.innerHTML = "<h2>How much was the global videogame market worth in 2018?</h2><form id='ques8' method='get'><input type='radio' name='569m'> $569 million <input type='radio' name='799b'> $799 billion <input type='radio' name='135m'> $135 billion <input type='radio' name='61b'> $61 billion </form><br>";
 
-        stopButt.innerHTML = "<h2>Done</h2>"
+        stopButt.innerHTML = "<h2>Done</h2>";
 
     });
 
@@ -97,22 +109,21 @@ $(document).ready(function () {
 
         console.log("Now we empty the start divs and fill the score divs.");
 
-        clearTimeout(purge);
+        // Clear the questions
+
+        purge();
 
         alert("Stop");
 
-        // Clear the questions
-
-        question1.innerHTML = "";
-        question2.innerHTML = "";
-        question3.innerHTML = "";
-        question4.innerHTML = "";
-        question5.innerHTML = "";
-        question6.innerHTML = "";
-        question7.innerHTML = "";
-        question8.innerHTML = "";       
-        stopButt.innerHTML = "";
         console.log("Stopped");
+
+        counter.innerHTML = "";
+
+        correctAnswers.innerHTML = "<h2>Correct: " +  + "</h2>";
+
+        incorrectAnswers.innerHTML = "<h2>Incorrect: " +  + "</h2>";
+
+        unAnswered.innerHTML = "<h2>Unanswered: " +  + "</h2>";
 
         // Then tabulate right and wrong answers, and unanswers
         
